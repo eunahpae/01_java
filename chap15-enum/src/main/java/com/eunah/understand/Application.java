0693db1bd721aad173158b23f2644858536dd4eb
EunahPae <eunahp86@gmail.com>
2024-05-24 17:01:36 +0900

    0524

--- chap15-enum/src/main/java/com/eunah/understand/Application.java
+++ chap15-enum/src/main/java/com/eunah/understand/Application.java
@@ -6,11 +6,11 @@ import java.util.Scanner;
 public class Application {
     public static void main(String[] args) {
 
-        Library library = new Library();
-        Scanner scr = new Scanner(System.in);
+        Library library = new Library(); // Library 클래스의 인스턴스 생성
+        Scanner scr = new Scanner(System.in); // 사용자 입력을 받기 위한 Scanner 객체 생성
 
 
-        while (true) {
+        while (true) { // 무한 루프 시작
             System.out.println("===================================");
             System.out.println("도서 관리 시스템");
             System.out.println("1. 도서 추가");
@@ -20,13 +20,13 @@ public class Application {
             System.out.println("5. 종료");
             System.out.println("옵션을 선택 해주세요");
 
-            int choice = 0;
+            int choice = 0; // 사용자가 선택한 옵션을 저장할 변수 초기화
 
             try {
-                choice = scr.nextInt();
-                scr.nextLine();
+                choice = scr.nextInt(); // 사용자가 입력한 정수 저장
+                scr.nextLine(); // 개행 문자 제거
 
-                switch (choice) {
+                switch (choice) { // 선택한 옵션에 따른 각 케이스 구문
                     case 1:
                         System.out.println("도서 제목을 입력 해주세요");
                         String title = scr.nextLine();
@@ -35,20 +35,20 @@ public class Application {
                         System.out.println("도서 등록 번호를 입력 해주세요");
                         int number = scr.nextInt();
                         scr.nextLine();
-                        String num = number + "";
-                        Genre genre = null;
+
+                        String num = number + ""; // 도서 등록 번호를 문자열로 변환
+                        Genre genre = null; // 장르 변수 초기화
                         do {
                             try {
                                 System.out.println("장르를 입력 해주세요 (FICTION, NONFICTION, SCIENCE, HISTORY, FANTASY, MYSTERY)");
                                 String genStr = scr.nextLine();
-                                genre = Genre.valueOf(genStr.toUpperCase());
+                                genre = Genre.valueOf(genStr.toUpperCase()); // 입력된 문자열을 대문자로 변환하여 Genre 열거형으로 변환
                             } catch (IllegalArgumentException e) {
-                                System.out.println("올바르지 않은 장르입니다. 다시 입력 해주세요");
+                                System.out.println("올바르지 않은 장르입니다. 다시 입력 해주세요"); // 잘못된 장르가 입력되었을 경우 메시지 출력
                             }
+                        } while (genre == null); // 올바른 장르가 입력될 때까지 반복
 
-                        } while (genre == null);
-
-                        boolean isTrue = library.addBook(new Book(title, author, num, genre));
+                        boolean isTrue = library.addBook(new Book(title, author, num, genre)); // 입력된 정보를 바탕으로 새로운 도서를 추가
                         if (isTrue) {
                             System.out.println("도서가 성공적으로 등록되었습니다.");
                         } else {
@@ -56,48 +56,43 @@ public class Application {
                         }
                         break;
                     case 2:
-                        System.out.println("보고 싶은 목록의 장르를 입력 하세요 (FICTION, NONFICTION, SCIENCE, HISTORY, FANTASY, MYSTERY)");
-                        Genre genre1 = null;
+                        System.out.println("보고 싶은 목록의 장르를 입력 하세요 (FICTION, NONFICTION, SCIENCE, HISTORY, FANTASY, MYSTERY)"); // 장르 입력 요청
+                        Genre genre1 = null; // 장르 변수를 초기화
                         do {
                             try {
                                 String geneStr = scr.nextLine();
-                                genre1 = Genre.valueOf(geneStr.toUpperCase());
+                                genre1 = Genre.valueOf(geneStr.toUpperCase()); // 입력된 문자열을 대문자로 변환하여 Genre 열거형으로 변환
                             } catch (IllegalArgumentException e) {
-                                System.out.println("올바르지 않은 장르입니다.");
+                                System.out.println("올바르지 않은 장르입니다."); // 잘못된 장르가 입력되었을 경우 메시지를 출력
                             }
-                        } while (genre1 == null);
-                        library.printBooksGenre(genre1);
+                        } while (genre1 == null); // 올바른 장르가 입력될 때까지 반복
+                        library.printBooksGenre(genre1); // 해당 장르의 도서 목록을 출력
                         break;
                     case 3:
-                        System.out.println("대여할 도서의 번호를 입력 해주세요");
-                        int number1 = scr.nextInt();
-                        scr.nextLine();
-                        String num1 = number1 + "";
-                        library.borrowBook(num1);
+                        System.out.println("대여할 도서의 번호를 입력 해주세요"); // 대여할 도서의 번호 입력 요청
+                        int number1 = scr.nextInt(); // 사용자가 입력한 도서 번호 저장
+                        scr.nextLine(); // 개행 문자 제거
+                        String num1 = number1 + ""; // 도서 번호를 문자열로 변환
+                        library.borrowBook(num1); // 도서 대여
                         break;
                     case 4:
                         System.out.println("반납할 도서의 번호를 입력 해주세요");
                         int number2 = scr.nextInt();
                         scr.nextLine();
                         String num2 = number2 + "";
-                        library.returnBook(num2);
+                        library.returnBook(num2); // 도서 반납
                         break;
                     case 5:
                         System.out.println("시스템을 종료합니다.");
-                        return;
+                        return; // 프로그램 종료
                     default:
                         System.out.println("잘못 선택하셨습니다.");
                         break;
-
                 }
             } catch (InputMismatchException e) {
                 System.out.println("정수를 입력 해주세요");
                 scr.nextLine();
             }
-
-
         }
-
-
     }
 }
--- chap15-enum/src/main/java/com/eunah/understand/Library.java
+++ chap15-enum/src/main/java/com/eunah/understand/Library.java
@@ -5,57 +5,55 @@ import java.util.HashMap;
 
 public class Library {
 
-    private HashMap<Genre, ArrayList<Book>> bookMap;
+    private HashMap<Genre, ArrayList<Book>> bookMap; // 장르별로 도서 목록을 관리할 HashMap
 
     // 생성자
     public Library() {
-        this.bookMap = new HashMap<>();
-        for (Genre genre : Genre.values()) {
-            bookMap.put(genre, new ArrayList<>());
+        this.bookMap = new HashMap<>(); // bookMap 초기화
+        for (Genre genre : Genre.values()) { // 모든 장르에 대해
+            bookMap.put(genre, new ArrayList<>()); // 각 장르에 빈 ArrayList 를 추가
         }
     }
 
     // 도서 추가 메소드
     public boolean addBook(Book book) {
-        for (ArrayList<Book> books : bookMap.values()) {
-            for (Book existBook : books) {
-                if (existBook.getTitle().equals(book.getTitle())) {
-                    System.out.println("이미 같은 제목의 도서가 존재합니다.");
-                    return false;
+        for (ArrayList<Book> books : bookMap.values()) { // 향상된 for문을 ㅗ든 장르의 도서 목록 탐색
+            for (Book existBook : books) { // 각 장르의 도서 목록에서 도서를 하나씩 확인
+                if (existBook.getTitle().equals(book.getTitle())) { // 동일한 제목의 도서가 있는지 확인
+                    System.out.println("이미 같은 제목의 도서가 존재합니다."); // 동일도서 존재시 메시지 출력
+                    return false; // 도서를 추가하지 않고 false를 반환
                 }
-                if (existBook.getNum().equals(book.getNum())) {
-                    System.out.println("이미 같은 번호의 도서가 존재합니다.");
-                    return false;
+                if (existBook.getNum().equals(book.getNum())) { // 동일한 번호의 도서가 있는지 확인
+                    System.out.println("이미 같은 번호의 도서가 존재합니다."); // 동일번호 존재시 메세지 출력
+                    return false; // 도서를 추가하지 않고 false를 반환
                 }
             }
         }
 
-        bookMap.get(book.getGenre()).add(book);
-        return true;
+        bookMap.get(book.getGenre()).add(book); // 해당 도서의 장르에 도서를 추가
+        return true; // 도서를 성공적으로 추가한 경우 true를 반환
     }
 
     // 특정 장르 도서 목록 출력 메소드
     public void printBooksGenre(Genre genre) {
-        ArrayList<Book> books = bookMap.get(genre);
-        System.out.println(genre + " 장르 도서 목록");
-        for (Book book : books) {
-            System.out.println(book.getTitle() + " " + book.getAuthor() + " " + book.getNum() + " " + book.isBorrowed());
+        ArrayList<Book> books = bookMap.get(genre); // 해당 장르의 도서 목록을 가져옴
+        System.out.println(genre + " 장르 도서 목록"); // 장르별 도서 목록 제목을 출력
+        for (Book book : books) { // 해당 장르의 도서 목록을 순회
+            System.out.println(book.getTitle() + " " + book.getAuthor() + " " + book.getNum() + " " + book.isBorrowed()); // 각 도서의 제목, 저자, 번호, 대여 상태를 출력
         }
-
     }
-
 
     // 도서 대여 메소드
     public void borrowBook(String num) {
-        for (ArrayList<Book> books : bookMap.values()) {
-            for (Book book : books) {
-                if (book.getNum().equals(num)) {
-                    book.borrowBook();
-                    return;
+        for (ArrayList<Book> books : bookMap.values()) { // 모든 장르의 도서 목록을 탐색
+            for (Book book : books) { // 각 장르의 도서 목록에서 도서를 하나씩 확인.
+                if (book.getNum().equals(num)) { // 입력된 번호와 일치하는 도서를 검색
+                    book.borrowBook(); // 도서 대여
+                    return; // 메소드 종료
                 }
             }
         }
-        System.out.println("해당 " + num + " 번의 도서를 찾을 수 없습니다.");
+        System.out.println("해당 " + num + " 번의 도서를 찾을 수 없습니다."); // 잘못된 메시지 출력
     }
 
     // 도서 반납 메소드
@@ -68,9 +66,6 @@ public class Library {
                 }
             }
         }
-        System.out.println("해당 " + num + " 번의 도서를 찾을 수 없습니다.");
-
+        System.out.println("해당 " + num + " 번의 도서를 찾을 수 없습니다."); // 잘못된 메시지 출력
     }
-
-
 }
